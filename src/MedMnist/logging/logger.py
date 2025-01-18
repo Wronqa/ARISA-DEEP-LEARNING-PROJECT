f2c4db14cf8caffa3f88142aaeb738d545af223b
pwgawzynski <pgawzynski.backup@gmail.com>
2025-01-18 19:50:10 +0100

    added log to app.log functionality

--- src/MedMnist/logging/logger.py
+++ src/MedMnist/logging/logger.py
@@ -1,5 +1,24 @@
 import logging
 
-def setup_logger():
-    logging.basicConfig(level=logging.INFO)
-    return logging.getLogger("MedMnist")
+def setup_logger(log_file="app.log"):
+    # Tworzenie loggera
+    logger = logging.getLogger("MedMnist")
+    logger.setLevel(logging.INFO)
+
+    # Tworzenie formatera
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+    # Tworzenie FileHandler do zapisywania logów do pliku
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setFormatter(formatter)
+
+    # Tworzenie StreamHandler do wyświetlania logów w konsoli
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    # Dodanie handlerów do loggera
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
+
