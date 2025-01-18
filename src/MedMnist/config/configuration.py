5eb2eef9006d0cac037c91eed88f7f5107feb42e
pwgawzynski <pgawzynski.backup@gmail.com>
2025-01-18 19:44:03 +0100

    added base setup for init test models and parameters optimalisation

--- src/MedMnist/config/configuration.py
+++ src/MedMnist/config/configuration.py
@@ -1,8 +1,13 @@
-params = {
-    "learning_rate_range": (1e-5, 1e-2),
-    "batch_size_options": [16, 32, 64],
-    "dropout_rate_range": (0.1, 0.5),
-    "filters_1_options": [16, 32, 64],
-    "filters_2_options": [64, 128, 256],
-    "dense_units_options": [64, 128, 256],
+optymized_params = {
+    "learning_rate_range": (1e-6, 1e-1),  # Rozszerzony zakres do testowania ekstremalnych wartości
+    "batch_size_options": [64, 128, 256, 512, 1024],  # Dodano więcej opcji batch size
+    "dropout_rate_range": (0.0, 0.7),  # Poszerzony zakres, w tym brak dropout
+    "filters_1_options": [8, 16, 32, 64],  # Dodano mniejsze wartości dla eksperymentów z prostszymi modelami
+    "filters_2_options": [32, 64, 128, 256, 512],  # Dodano większe wartości dla bardziej złożonych modeli
+    "filters_3_options": [128, 256, 512],  # Dodano dla modeli z trzecią warstwą Conv2D
+    "dense_units_options": [32, 64, 128, 256, 512],  # Poszerzony zakres dla gęstych warstw
+    "activation_functions": ["relu", "tanh", "elu", "swish"],  # Opcje różnych funkcji aktywacji
+    "optimizer_options": ["adam", "sgd", "rmsprop", "adamax"],  # Różne optymalizatory do testowania
+    "epochs_range": (50, 100, 200),  # Zakres liczby epok do trenowania
+    "early_stopping_patience": [5, 10],  # Dłuższa cierpliwość dla wczesnego zatrzymywania
 }
